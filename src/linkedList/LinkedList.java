94a36d173667b9f9b3527199e16fdfa27e04aa11
akhsa234 <saremi.bah2000@gmail.com>
2024-04-10 20:11:54 +0430

    git commit -m "removeLast method"

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -32,37 +32,59 @@ public class LinkedList {
         return first == null;
     }
 
-    public int indexOf(int item){
-        int index=0;
+    public int indexOf(int item) {
+        int index = 0;
         var current = first;
-        if(current != null){
-             if(current.value == item) return index;
-             else{
-                 current = current.next;
-                 index++;
-             }
+        if (current != null) {
+            if (current.value == item) return index;
+            else {
+                current = current.next;
+                index++;
+            }
         }
         return -1;
     }
 
-    public boolean contains(int item){
+    public boolean contains(int item) {
         return indexOf(item) != -1;
     }
 
-    public void removeFirst(){
+    public void removeFirst() {
 
-        if(isEmpty()){
+        if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        if(first==last){
-            first=last=null;
+        if (first == last) {
+            first = last = null;
             return;
         }
         var second = first.next;
         first.next = null;
-        first=second;
+        first = second;
     }
 
+    public void removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        var previou = getPreviousNode(last);
+        last = previou;
+        previou.next = null;
+    }
+
+    private Node getPreviousNode(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
 
     private class Node {
 
